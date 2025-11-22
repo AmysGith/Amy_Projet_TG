4085830a907ee32e12ea0012cc50e9ef721e1ab3
amy-andriamalala <amynomiharintsoa@gmail.com>
2025-11-22 11:45:11 +0400

    Insertion

--- Scripts/table_creation.sql
+++ Scripts/table_creation.sql
@@ -1,4 +1,3 @@
-DROP TABLE IF EXISTS Contrainte CASCADE;
 DROP TABLE IF EXISTS Arete CASCADE;
 DROP TABLE IF EXISTS Sommet CASCADE;
 
@@ -9,16 +8,28 @@ CREATE TABLE Sommet (
 
 CREATE TABLE Arete (
     IdArete SERIAL PRIMARY KEY,
-    Distance INT NOT NULL,
     ptD INT NOT NULL,
     ptA INT NOT NULL,
+    Distance INT NOT NULL,
+    Contrainte INT DEFAULT 0,
     CONSTRAINT fk_ptD FOREIGN KEY (ptD) REFERENCES Sommet(IdSommet) ON DELETE CASCADE,
     CONSTRAINT fk_ptA FOREIGN KEY (ptA) REFERENCES Sommet(IdSommet) ON DELETE CASCADE
 );
 
-CREATE TABLE Contrainte (
-    IdContrainte SERIAL PRIMARY KEY,
-    Value INT NOT NULL,
-    AreteId INT,
-    CONSTRAINT fk_arete_contrainte FOREIGN KEY (AreteId) REFERENCES Arete(IdArete) ON DELETE CASCADE
-);
+INSERT INTO Sommet (NomSommet) VALUES
+('A'),
+('B'),
+('C'),
+('D');
+
+INSERT INTO Arete (ptD, ptA, Distance, Contrainte) VALUES
+(1, 2, 2, 7),
+(1, 3, 5, 0),
+(2, 1, 2, 7),
+(2, 3, 1, 0),
+(2, 4, 4, 0),
+(3, 1, 5, 0),
+(3, 2, 1, 0),
+(3, 4, 2, 0),
+(4, 2, 4, 0),
+(4, 3, 2, 0);
